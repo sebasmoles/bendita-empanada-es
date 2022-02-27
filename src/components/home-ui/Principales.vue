86157b8054f7043875d77d04081adde01a8629dd
Sebastian <roshfeltsemo@gmail.com>
2022-02-26 21:46:21 -0500

    estilo(principales): agregar estilos, no responsive

--- src/components/home-ui/Principales.vue
+++ src/components/home-ui/Principales.vue
@@ -1,20 +1,51 @@
 <template>
-	<h2>Productos Principales</h2>
-	<div>
-		<div>
-			<img src="" alt="" />
-			<p>Parrafo</p>
+	<section class="principales">
+		<h2 class="principales-titulo">Productos principales</h2>
+		<div class="principales-elementos">
+			<div>
+				<img
+					src="../../assets/empanada-hero.jpeg"
+					alt="Imagen producto"
+					class="principales-imagen"
+				/>
+				<p class="principales-texto">
+					Vivamus porta, quam semper euismod molestie, enim eros
+					posuere dui, ac aliquet lectus elit ut sapien. Pellentesque
+					ligula leo, convallis quis pulvinar vel, lacinia a turpis.
+				</p>
+			</div>
+			<div>
+				<img
+					src="../../assets/empanada-hero.jpeg"
+					alt="Imagen producto"
+					class="principales-imagen"
+				/>
+				<p class="principales-texto">
+					Vivamus porta, quam semper euismod molestie, enim eros
+					posuere dui, ac aliquet lectus elit ut sapien. Pellentesque
+					ligula leo, convallis quis pulvinar vel, lacinia a turpis.
+				</p>
+			</div>
+			<div>
+				<img
+					src="../../assets/empanada-hero.jpeg"
+					alt="Imagen producto"
+					class="principales-imagen"
+				/>
+				<p class="principales-texto">
+					Vivamus porta, quam semper euismod molestie, enim eros
+					posuere dui, ac aliquet lectus elit ut sapien. Pellentesque
+					ligula leo, convallis quis pulvinar vel, lacinia a turpis.
+				</p>
+			</div>
 		</div>
-		<div>
-			<img src="" alt="" />
-			<p>Parrafo</p>
-		</div>
-		<div>
-			<img src="" alt="" />
-			<p>Parrafo</p>
-		</div>
-	</div>
-	<button>Boton</button>
+		<button
+			class="principales-boton"
+			@click="this.$router.push('/productos')"
+		>
+			Ver más
+		</button>
+	</section>
 </template>
 
 <script>
@@ -23,4 +54,35 @@
 	};
 </script>
 
-<style></style>
+<style scoped>
+	.principales {
+		text-align: center;
+		padding: 4rem 1rem;
+		background-color: #282828;
+		color: white;
+	}
+	.principales-titulo {
+		margin-bottom: 2rem;
+	}
+	.principales-elementos {
+		display: flex;
+		margin-bottom: 2rem;
+	}
+	.principales-elementos div:not(:last-child) {
+		margin-right: 6rem;
+	}
+	.principales-imagen {
+		max-width: 100%;
+	}
+	.principales-texto {
+		text-align: left;
+	}
+	.principales-boton {
+		background: white;
+		color: black;
+	}
+	.principales-boton:hover {
+		background: gray;
+		color: white;
+	}
+</style>
