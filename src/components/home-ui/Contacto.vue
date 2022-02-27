334416f121ef2cc897e75c53dc1ba4c1eb779a1d
Sebastian <roshfeltsemo@gmail.com>
2022-02-26 23:14:38 -0500

    estilo(contacto): añadir estilos, no responsive

--- src/components/home-ui/Contacto.vue
+++ src/components/home-ui/Contacto.vue
@@ -1,13 +1,44 @@
 <template>
-	<section id="contacto">
-		<h2>Contacto</h2>
-		<div>
-			<p>Parrafo</p>
-			<form>
-				<input type="text" />
-				<input type="text" />
-				<input type="text" />
-				<input type="textarea" />
+	<section id="contacto" class="contacto">
+		<h2 class="contacto-titulo">Contacto</h2>
+		<div class="contacto-elementos">
+			<p class="contacto-texto">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+				nisl nisl, commodo at auctor quis, aliquet sed felis. Praesent
+				neque nibh, efficitur placerat semper non, feugiat porttitor
+				augue. Sed interdum elit nec nulla pulvinar, at fermentum dolor
+				tempus. Donec auctor urna ac sem faucibus, ac pulvinar elit
+				convallis. Nam egestas convallis metus nec maximus. Nulla eget
+				malesuada urna. Phasellus condimentum, dolor at dapibus pretium,
+				justo massa sollicitudin leo, in dictum ex mauris nec libero.
+				Nam dictum turpis orci, et placerat neque interdum porta.
+				Integer egestas, orci molestie luctus rhoncus, sem dui
+				ullamcorper ligula, sed gravida ex urna a ligula. Nam vel
+				dapibus dui. Cras sem justo, euismod ut tristique a, accumsan
+				vestibulum magna.
+			</p>
+			<form class="contacto-formulario">
+				<div class="contacto-formulario-grupo">
+					<input type="text" placeholder="Nombre completo" required />
+				</div>
+				<div class="contacto-formulario-grupo">
+					<input
+						type="text"
+						placeholder="Correo electrónico"
+						required
+					/>
+				</div>
+				<div class="contacto-formulario-grupo">
+					<input
+						type="text"
+						placeholder="Teléfono de contacto"
+						required
+					/>
+				</div>
+				<div class="contacto-formulario-grupo">
+					<textarea placeholder="¿Cómo podemos ayudarte?" required />
+				</div>
+				<button type="submit">Enviar</button>
 			</form>
 		</div>
 	</section>
@@ -19,4 +50,45 @@
 	};
 </script>
 
-<style></style>
+<style scoped>
+	.contacto {
+		padding: 4rem 2rem;
+		text-align: center;
+	}
+	.contacto-titulo {
+		margin-bottom: 2rem;
+	}
+	.contacto-elementos {
+		display: flex;
+		align-items: flex-start;
+		max-width: 100%;
+	}
+	.contacto-texto {
+		margin: 0 2rem 0 0;
+		text-align: left;
+		flex: 1;
+	}
+	.contacto-formulario {
+		flex: 1;
+	}
+	.contacto-formulario button {
+		width: 100%;
+	}
+	.contacto-formulario-grupo {
+		min-height: 100%;
+		margin-bottom: 1rem;
+	}
+	.contacto-formulario-grupo input {
+		width: 100%;
+		height: 42px;
+		padding: 1rem;
+		border: 1px solid #ccc;
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+	}
+	.contacto-formulario-grupo textarea {
+		width: 100%;
+		padding: 1rem;
+		border: 1px solid #ccc;
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+	}
+</style>
