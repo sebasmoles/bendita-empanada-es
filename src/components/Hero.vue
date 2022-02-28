e2d8bf7f4d36598bb189f251c05b6348590c18a2
Sebastian <roshfeltsemo@gmail.com>
2022-02-28 01:15:55 -0500

    estilo(hero): añadir estilos, no responsive

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,19 +1,69 @@
 <template>
-	<img
-		src="../assets/empanada-hero.jpeg"
-		alt="Empanadas"
-		class="imagen-hero"
-	/>
+	<section class="hero">
+		<swiper :pagination="{ clickable: true }" :slides-per-view="1">
+			<swiper-slide>
+				<img
+					src="../assets/empanada-hero.jpeg"
+					alt="Empanadas"
+					class="imagen-hero"
+				/>
+			</swiper-slide>
+			<swiper-slide>
+				<img
+					src="../assets/empanada-hero.jpeg"
+					alt="Empanadas"
+					class="imagen-hero"
+				/>
+			</swiper-slide>
+			<swiper-slide>
+				<img
+					src="../assets/empanada-hero.jpeg"
+					alt="Empanadas"
+					class="imagen-hero"
+				/>
+			</swiper-slide>
+		</swiper>
+	</section>
 </template>
 
 <script>
+	// import Swiper core and required modules
+	import SwiperCore, { Pagination } from "swiper";
+
+	// Import Swiper Vue.js components
+	import { Swiper } from "swiper/vue/swiper";
+	import { SwiperSlide } from "swiper/vue/swiper-slide";
+
+	// Import Swiper styles
+	import "swiper/swiper.min.css";
+	import "swiper/modules/pagination/pagination.min.css";
+	SwiperCore.use([Pagination]);
+
 	export default {
 		name: "Hero",
+		components: {
+			Swiper,
+			SwiperSlide,
+		},
 	};
 </script>
 
-<style scoped>
+<style>
 	.imagen-hero {
 		width: 100%;
+		height: 50vh;
+		object-fit: cover;
+	}
+	.swiper-pagination-bullet {
+		width: 20px;
+		height: 20px;
+		display: inline-block;
+		border-radius: 50%;
+		background: white;
+		opacity: 0.5;
+	}
+	.swiper-pagination-bullet-active {
+		opacity: 1;
+		background: white;
 	}
 </style>
