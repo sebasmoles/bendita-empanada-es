b865a9ed059aa66d5ef7b414cde56992236277e7
Sebastian <roshfeltsemo@gmail.com>
2022-04-02 10:53:49 -0500

    fix(server): build path

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 3000;
-const distPath = path.join(__dirname, "dist");
+const distPath = path.join(__dirname, "..", "dist");
 
 app.use(express.static(distPath));
 
